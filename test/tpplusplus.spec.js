7be4594dd25f01f478d2f5e0cc0cc7e767f154a3
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-03-05 08:36:16 -0600

    Add in first files

---
+++ test/tpplusplus.spec.js
@@ -0,0 +1,12 @@
+// var results = document.getElementById("qunit-fixture");
+
+module("main", {
+    setup: function() {
+        
+    }
+});
+
+test("when a tp button is on the page", function() {
+    ok(true);
+    // ok(results.firstChild.className.indexOf("btn-bookmarklet") > 0);
+});
