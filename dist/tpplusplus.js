4b77511d142a80fbbc846e13a5f45ff2e44e1948
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-03-27 10:44:03 -0500

    Add object.create polyfill

--- dist/tpplusplus.js
+++ dist/tpplusplus.js
@@ -4,7 +4,15 @@
 ;(function() {
 "use strict";
 // var slice = Array.prototype.slice;
-
+if (typeof Object.create !== "function") {
+    (function () {
+        var F = function () {};
+        Object.create = function (o) {
+            F.prototype = o;
+            return new F();
+        };
+    })();
+}
 var TP = {};
 
 TP.Core = {};
